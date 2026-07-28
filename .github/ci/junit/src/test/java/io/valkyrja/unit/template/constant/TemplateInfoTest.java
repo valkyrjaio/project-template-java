e24b567ea8e779202301cd6cda1d34aa378fbb29
Melech Mizrachi <melechmizrachi@gmail.com>
2026-07-27 20:38:01 -0700

    [Template] Add the TemplateInfo constant class and its test.

---
+++ .github/ci/junit/src/test/java/io/valkyrja/unit/template/constant/TemplateInfoTest.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the Valkyrja Framework package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package io.valkyrja.unit.template.constant;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import io.valkyrja.template.constant.TemplateInfo;
+import org.junit.jupiter.api.Test;
+
+/** Tests for {@link TemplateInfo}. */
+final class TemplateInfoTest {
+
+    @Test
+    void versionIsSet() {
+        assertNotEquals("", TemplateInfo.VERSION);
+    }
+
+    @Test
+    void versionBuildDateTimeIsSet() {
+        assertNotEquals("", TemplateInfo.VERSION_BUILD_DATE_TIME);
+    }
+}
