afef994239145a8003ce76fd6bfe6674d069288b
MelechMizrachi <1179171+MelechMizrachi@users.noreply.github.com>
2026-08-03 12:16:09 +0000

    [Dependency] build: Update Gradle dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     java
     `maven-publish`
     signing
-    id("com.github.ben-manes.versions") version "0.58.0"
+    id("com.github.ben-manes.versions") version "0.59.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
 }
 
